2f6a7e5d9255eb35632c9bfbc48781573f969825
Sid-Designs <siddheshmhaskar05@gmail.com>
2025-02-23 19:17:13 +0530

    Add Product With Cloudinary

--- src/components/SizeSelector.jsx
+++ src/components/SizeSelector.jsx
@@ -7,7 +7,7 @@ const SizeSelector = ({ onSizeChange }) => {  // Receive function from parent
   const [selectedMin, setSelectedMin] = useState(null);
   const [selectedMax, setSelectedMax] = useState(null);
 
-  const minSizeRange = [...Array(10)].map((_, i) => i + 1); // 7 to 16
+  const minSizeRange = [...Array(10)].map((_, i) => i + 1); // 1 to 10
   const maxSizeRange = [...Array(10)].map((_, i) => i + 15); // 15 to 24
 
   const handleMinSizeChange = (size) => {
@@ -31,6 +31,8 @@ const SizeSelector = ({ onSizeChange }) => {  // Receive function from parent
         <div
           className="selectTrigger w-[200px] flex justify-between items-center p-2 cursor-pointer"
           onClick={() => setIsSizeDropdownOpen(!isSizeDropdownOpen)}
+          aria-haspopup="true"
+          aria-expanded={isSizeDropdownOpen}
         >
           {selectedMin && selectedMax ? `${selectedMin} - ${selectedMax}` : "Select Size"}
           {isSizeDropdownOpen ? <ChevronUp className="pr-2" /> : <ChevronDown className="pr-2" />}
@@ -41,7 +43,7 @@ const SizeSelector = ({ onSizeChange }) => {  // Receive function from parent
             {step === 1 && (
               <>
                 <div className="font-semibold p-2 py-3 bg-[var(--accent-color)] text-[var(--background-color)]">Select Min Range</div>
-                <div className="flex flex-col space-y-2" style={{borderTop: "2px solid #efefef"}}>
+                <div className="flex flex-col space-y-2" style={{ borderTop: "2px solid #efefef" }}>
                   {minSizeRange.map((size) => (
                     <div
                       key={size}
@@ -58,7 +60,7 @@ const SizeSelector = ({ onSizeChange }) => {  // Receive function from parent
             {step === 2 && (
               <>
                 <div className="font-semibold p-2 py-3 bg-[var(--accent-color)] text-[var(--background-color)]">Select Max Range</div>
-                <div className="flex flex-col space-y-2" style={{borderTop: "2px solid #efefef"}}>
+                <div className="flex flex-col space-y-2" style={{ borderTop: "2px solid #efefef" }}>
                   {maxSizeRange
                     .filter((size) => size >= selectedMin) // Ensure max is >= min
                     .map((size) => (
