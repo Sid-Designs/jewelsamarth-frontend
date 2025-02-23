128a75fbda48d28d3af605629efa5c5e08160c1a
Sid-Designs <siddheshmhaskar05@gmail.com>
2025-02-23 20:41:48 +0530

    Single Product

--- src/components/SingleCollection.jsx
+++ src/components/SingleCollection.jsx
@@ -52,129 +52,128 @@ const SingleCollection = () => {
   };
 
   return (
-    <div className="singleProd flex flex-col justify-center items-start px-8">
-      {/* Left Image Section */}
-      <div className="singleProdImg w-full flex py-8">
-        <ul className="singleSubProdImg flex flex-col justify-start items-center pt-2 gap-4">
-          {subImages.map((img, index) => (
-            <li
-              key={index}
-              onClick={() => handleImageClick(img, index)}
-              className="cursor-pointer transition-transform transform hover:scale-105"
+    <div className="singleProd flex justify-center items-start px-8">
+      <div className="flex flex-col">
+        {/* Left Image Section */}
+        <div className="singleProdImg w-full flex py-8">
+          <ul className="singleSubProdImg flex flex-col justify-start items-center pt-2 gap-4">
+            {subImages.map((img, index) => (
+              <li
+                key={index}
+                onClick={() => handleImageClick(img, index)}
+                className="cursor-pointer transition-transform transform hover:scale-105"
+              >
+                <img
+                  src={img}
+                  alt={`Sub ${index}`}
+                  className="w-full rounded-[20px] h-full object-cover transition-opacity duration-300"
+                />
+              </li>
+            ))}
+          </ul>
+
+          <div className="singleMainProdImg w-full mx-12">
+            <img
+              src={mainImage}
+              alt="Product"
+              className="w-full h-auto rounded-lg object-cover transition-opacity duration-500 ease-in-out"
+            />
+          </div>
+        </div>
+
+        {/* Right Details Section */}
+        <div className="singleProdDtl w-full flex justify-center items-start flex-col p-8">
+          <div className="singleProdTitle font-bold text-2xl">{product.name}</div>
+          <div className="singleProdPrice flex gap-2 items-center mb-4">
+            <div className="salePrice text-2xl pr-2">₹{product.saleprice}</div>
+            <div className="regPrice text-md line-through text-gray-500">
+              ₹{product.regprice}
+            </div>
+            <div className="prodTax text-gray-500">Incl. of all taxes</div>
+          </div>
+
+          <div>
+            <span className="text-sm">Availability:</span>
+            <span
+              className={`text-sm ${product.stock > 0 ? "text-green-700" : "text-red-700"
+                }`}
             >
-              <img
-                src={img}
-                alt={`Sub ${index}`}
-                className="w-full rounded-[20px] h-full object-cover transition-opacity duration-300"
-              />
+              {product.stock > 0 ? "In Stock" : "Out of Stock"}
+            </span>
+          </div>
+
+          {/* Delivery Time */}
+          <div className="singleProdPincode my-4 w-[50%]">
+            <div className="text-lg">Estimated Delivery Time</div>
+            <div className="my-2 flex gap-4">
+              <input type="number" min={1} className="p-2 pl-4 w-full" />
+              <button className="text-md px-4">Check</button>
+            </div>
+          </div>
+
+          {/* Add to Cart & Buy Now */}
+          <div className="singleProdBtns flex w-[70%] my-4 px-4 pl-0">
+            <div className="addToCartBtn w-full text-center py-2 mx-4 ml-0">
+              Add To Cart
+            </div>
+            <div className="buyNowBtn w-full text-center py-2 mx-4">
+              Buy Now
+            </div>
+          </div>
+
+          {/* Info Section */}
+          <ul className="singleProdInfo flex gap-4 my-4 w-full">
+            <li className="w-fit flex gap-2 items-center">
+              <RotateCw className="text-gray-500" size={20} />
+              Delivery & Return
             </li>
-          ))}
-        </ul>
+            <li className="w-fit flex gap-2 items-center">
+              <CircleHelp className="text-gray-500" size={20} />
+              Ask a Question
+            </li>
+          </ul>
 
-        <div className="singleMainProdImg w-full mx-12">
-          <img
-            src={mainImage}
-            alt="Product"
-            className="w-full h-auto rounded-lg object-cover transition-opacity duration-500 ease-in-out"
-          />
-        </div>
-      </div>
-
-      {/* Right Details Section */}
-      <div className="singleProdDtl w-full flex justify-center items-start flex-col p-8">
-        <div className="singleProdTitle font-bold text-2xl">{product.name}</div>
-        <div className="singleProdPrice flex gap-2 items-center mb-4">
-          <div className="salePrice text-2xl pr-2">₹{product.saleprice}</div>
-          <div className="regPrice text-md line-through text-gray-500">
-            ₹{product.regprice}
+          <div className="w-full mb-4 flex gap-2 items-center">
+            <History className="text-gray-500" size={20} />
+            Estimated Delivery:{" "}
+            <span className="text-gray-500">15TH Feb - 20TH Feb</span>
           </div>
-          <div className="prodTax text-gray-500">Incl. of all taxes</div>
+
+          {/* Features */}
+          <ul className="singleProdFeature flex justify-around my-4 w-full py-8">
+            <li className="flex flex-col justify-center items-center gap-2 text-center leading-none text-sm pointer-events-none">
+              <Truck className="text-[var(--primary-color)]" size={24} />
+              Fast <br /> Shipping
+            </li>
+            <li className="flex flex-col justify-center items-center gap-2 text-center leading-none text-sm pointer-events-none">
+              <Gem className="text-[var(--primary-color)]" size={24} />
+              925 Pure <br /> Silver
+            </li>
+            <li className="flex flex-col justify-center items-center gap-2 text-center leading-none text-sm pointer-events-none">
+              <ShieldCheck className="text-[var(--primary-color)]" size={24} />
+              Secure <br /> Payment
+            </li>
+            <li className="flex flex-col justify-center items-center gap-2 text-center leading-none text-sm pointer-events-none">
+              <CalendarSync className="text-[var(--primary-color)]" size={24} />
+              15 Days <br /> Returns
+            </li>
+          </ul>
         </div>
-
-        <div>
-          <span className="text-sm">Availability:</span>
-          <span
-            className={`text-sm ${
-              product.stock > 0 ? "text-green-700" : "text-red-700"
-            }`}
-          >
-            {product.stock > 0 ? "In Stock" : "Out of Stock"}
-          </span>
-        </div>
-
-        {/* Delivery Time */}
-        <div className="singleProdPincode my-4 w-[50%]">
-          <div className="text-lg">Estimated Delivery Time</div>
-          <div className="my-2 flex gap-4">
-            <input type="number" min={1} className="p-2 pl-4 w-full" />
-            <button className="text-md px-4">Check</button>
-          </div>
-        </div>
-
-        {/* Add to Cart & Buy Now */}
-        <div className="singleProdBtns flex w-[70%] my-4 px-4 pl-0">
-          <div className="addToCartBtn w-full text-center py-2 mx-4 ml-0">
-            Add To Cart
-          </div>
-          <div className="buyNowBtn w-full text-center py-2 mx-4">
-            Buy Now
-          </div>
-        </div>
-
-        {/* Info Section */}
-        <ul className="singleProdInfo flex gap-4 my-4 w-full">
-          <li className="w-fit flex gap-2 items-center">
-            <RotateCw className="text-gray-500" size={20} />
-            Delivery & Return
-          </li>
-          <li className="w-fit flex gap-2 items-center">
-            <CircleHelp className="text-gray-500" size={20} />
-            Ask a Question
-          </li>
-        </ul>
-
-        <div className="w-full mb-4 flex gap-2 items-center">
-          <History className="text-gray-500" size={20} />
-          Estimated Delivery:{" "}
-          <span className="text-gray-500">15TH Feb - 20TH Feb</span>
-        </div>
-
-        {/* Features */}
-        <ul className="singleProdFeature flex justify-around my-4 w-full py-8">
-          <li className="flex flex-col justify-center items-center gap-2 text-center leading-none text-sm pointer-events-none">
-            <Truck className="text-[var(--primary-color)]" size={24} />
-            Fast <br /> Shipping
-          </li>
-          <li className="flex flex-col justify-center items-center gap-2 text-center leading-none text-sm pointer-events-none">
-            <Gem className="text-[var(--primary-color)]" size={24} />
-            925 Pure <br /> Silver
-          </li>
-          <li className="flex flex-col justify-center items-center gap-2 text-center leading-none text-sm pointer-events-none">
-            <ShieldCheck className="text-[var(--primary-color)]" size={24} />
-            Secure <br /> Payment
-          </li>
-          <li className="flex flex-col justify-center items-center gap-2 text-center leading-none text-sm pointer-events-none">
-            <CalendarSync className="text-[var(--primary-color)]" size={24} />
-            15 Days <br /> Returns
-          </li>
-        </ul>
       </div>
 
       {/* Tabs Section */}
       <div className="w-full max-w-6xl mx-auto">
         <div className="relative flex justify-center items-center gap-8 text-xl py-4 pb-0 border-b">
           <div
-            className={`cursor-pointer px-4 py-2 ${
-              activeTab === "description" ? "text-[var(--accent-color)]" : ""
-            }`}
+            className={`cursor-pointer px-4 py-2 ${activeTab === "description" ? "text-[var(--accent-color)]" : ""
+              }`}
             onClick={() => setActiveTab("description")}
           >
             Description
           </div>
           <div
-            className={`cursor-pointer px-4 py-2 ${
-              activeTab === "reviews" ? "text-[var(--accent-color)]" : ""
-            }`}
+            className={`cursor-pointer px-4 py-2 ${activeTab === "reviews" ? "text-[var(--accent-color)]" : ""
+              }`}
             onClick={() => setActiveTab("reviews")}
           >
             Reviews
